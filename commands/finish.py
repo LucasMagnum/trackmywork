bc72b5438bc0054b8d56ce974c45d9a2963f0eb8
Lucas Magnum <lucasmagnum2@gmail.com>
2018-08-02 22:53:27 +0200

    Add docstring and parameter to finish command

--- commands/finish.py
+++ commands/finish.py
@@ -2,6 +2,21 @@ import click
 
 
 @click.command()
-def command():
-    """Finish command"""
-    click.echo("Finish command")
+@click.argument('activity_id', required=False)
+def command(activity_id):
+    """
+    We should save when we `finish` the task,
+    that way is possible to keep track of the time it took between the `start` and `finish`.
+
+    $ trackmywork finish
+
+    You successfully finished the last task #02
+
+    $ trackmywork finish #02
+
+    You successfully finished the task #02
+    """
+    if not activity_id:
+        click.echo("You successfully finished the last task #02")
+    else:
+        click.echo(f"You successfully finished the task '{activity_id}'.")
